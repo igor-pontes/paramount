db0e36103f63a4cc0badcb4fef5b89258ec27a3d
fafuja <46684536+fafuja@users.noreply.github.com>
2023-11-27 17:47:44 +0000

    Update playerctl.lua

--- files/config/awesome/notifications/playerctl.lua
+++ files/config/awesome/notifications/playerctl.lua
@@ -1,8 +1,18 @@
 local naughty = require("naughty")
 local playerctl_daemon = require("signal.playerctl")
 
+local is_playing = false
+
+playerctl_daemon:connect_signal("playback_status", function(_, playing, _)
+	if playing then
+		is_playing = true
+	else
+		is_playing = false
+	end
+end)
+
 playerctl_daemon:connect_signal("metadata", function(_, title, artist, album_path, album, new, player_name)
-	if new == true then
+	if new == true and is_playing then
 		naughty.notify({
 			app_name = "MUSIC",
 			title = title,
